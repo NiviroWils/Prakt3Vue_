0e60418d5c01c93f822d2639d790e70a5b2318a8
gr412_vokan <ksu280705@yandex.ru>
2024-02-22 09:05:41 +0700

    Определение начальных значений

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let app = new Vue({
+    el: '#app',
+    data() {
+        return {
+            newEntry: {
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: new Date().toLocaleString(),
+                lastModified: null,
+                reason: null,
+                overdue: false,
+                tasks: [],
+            },
+            stages: [
+                { name: 'To Do', entries: [] }, //определение столбцов
+                { name: 'In Progress', entries: [] },
+                { name: 'Testing', entries: [] },
+                { name: 'Completed', entries: [] }
+            ],
+            editedEntry: null,
+            editedEntryIndex: null,
+            editedStageIndex: null,
+            returnHistory: {},
+            newTaskItem: [],
+        }
+    },
+    methods: {}});
