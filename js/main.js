3d3e0a24d3b398d5ad4e8a5cfe8107fc4eb91c3d
gr412_vokan <ksu280705@yandex.ru>
2024-02-22 09:15:56 +0700

    Создана функция добавления задачи, а также проведена работа с html файлом

--- js/main.js
+++ js/main.js
@@ -26,5 +26,31 @@ let app = new Vue({
         }
     },
     methods: {
-
-    }});
+        addEntry() {
+            if (!this.newEntry.title || !this.newEntry.description || !this.newEntry.deadline) {
+                alert('Please fill in all fields');
+                return;
+            }
+            if (new Date(this.newEntry.deadline) <= new Date(new Date().setDate(new Date().getDate()))) {
+                alert('Invalid deadline date (should be at least tomorrow)');
+                return;
+            }
+            if (this.newEntry.tasks.length > 0) {
+                this.newEntry.tasks = this.newEntry.tasks
+                    .filter(task => task.trim() !== '')
+                    .map(task => ({ name: task, completed: false }));
+            }
+            this.stages[0].entries.push({ ...this.newEntry });
+            this.resetNewEntry();
+        },
+        resetNewEntry() {
+            this.newEntry = {
+                title: '',
+                description: '',
+                deadline: '',
+                createdAt: new Date().toLocaleString(),
+                lastModified: null,
+                tasks: []
+            };
+        },
+    }})
